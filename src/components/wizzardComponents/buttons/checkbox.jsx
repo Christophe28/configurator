9d2cdaf5bd9daa56b8e4b77cfeb94b955bd3f519
christophe <christophebuffe@gmail.com>
2022-05-02 08:48:27 +0200

    dynamical defaultChecked, start is ok, next step change value of state

--- src/components/wizzardComponents/buttons/checkbox.jsx
+++ src/components/wizzardComponents/buttons/checkbox.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Checkbox = ({data}) => {
+const Checkbox = ({ data, checked }) => {
     return (
         <div>
             {
-                data.map((dataElement) => (
+                data.map((dataElement, index) => (
                     <div key={dataElement}>
-                        <input type="checkbox" id={dataElement} name={dataElement}/>
+                        <input 
+                            type="checkbox" 
+                            id={dataElement} 
+                            name={dataElement} 
+                            defaultChecked={checked[index]}
+                            onChange={(e) => console.log(e.target.checked)}
+                        />
                         <label htmlFor={dataElement}>{dataElement}</label>
                     </div>
                 ))
--- src/functions/saveUserColor.js
+++
@@ -1,7 +0,0 @@
-const saveUserColor = (e, colorName, hexadecimalColor) => {
-    if(e.target.value === colorName) {
-        setColor(hexadecimalColor);
-    } 
-}
-
-export default saveUserColor
