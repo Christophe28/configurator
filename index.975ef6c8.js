63e5a202d944d660a8e96b44dbb04892de44bf65
christophe <christophebuffe@gmail.com>
2022-05-16 15:21:36 +0200

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -24541,7 +24541,7 @@ var _exportAsImageDefault = parcelHelpers.interopDefault(_exportAsImage);
 var _updateIndex = require("../functions/updateIndex");
 var _updateIndexDefault = parcelHelpers.interopDefault(_updateIndex);
 var _s = $RefreshSig$();
-console.log("nouveau test so testyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
+console.log("nouveau test so testy");
 const Wizzard = ()=>{
     _s();
     const [currentWizardStep, setCurrentWizardStep] = _react.useState(0);
