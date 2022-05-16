06d6da27deeeee902cff5de03a58ffca5ebf0162
christophe <christophebuffe@gmail.com>
2022-05-16 15:21:13 +0200

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -1016,7 +1016,7 @@ var _app = require("./app");
 var _appDefault = parcelHelpers.interopDefault(_app);
 var _styleScss = require("./style/style.scss");
 _reactDomDefault.default.render(/*#__PURE__*/ _jsxDevRuntime.jsxDEV(_reactRouterDom.BrowserRouter, {
-    basename: "/configurator",
+    basename: "configurator",
     children: /*#__PURE__*/ _jsxDevRuntime.jsxDEV(_appDefault.default, {}, void 0, false, {
         fileName: "src/index.js",
         lineNumber: 9,
