9fb00e0fa4b7df63216b52cd496b956c2f552a7f
christophe <christophebuffe@gmail.com>
2022-05-17 10:12:24 +0200

    clean push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./app";
 import "./style/style.scss";
