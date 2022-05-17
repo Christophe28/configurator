7ace0d5c8015da1b65e4e757566954129720564f
christophe <christophebuffe@gmail.com>
2022-05-17 10:30:48 +0200

    Use lookalike swapi config to deploy on gh pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./app";
 import "./style/style.scss";
 
 ReactDOM.render(
-    <Router>
+    <Router basename={process.env.BASENAME}>
         <App />
     </Router>,
     document.getElementById("root")
