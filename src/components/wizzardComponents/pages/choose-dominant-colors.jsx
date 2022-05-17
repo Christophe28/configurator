ffea27f89c39b73d7af6a555b63748336a0dd614
christophe <christophebuffe@gmail.com>
2022-05-17 15:24:45 +0200

    button selected big size, not selected opacity 0.45

--- src/components/wizzardComponents/pages/choose-dominant-colors.jsx
+++ src/components/wizzardComponents/pages/choose-dominant-colors.jsx
@@ -2,13 +2,7 @@
 import React from "react";
 
 const ChooseDominantColor = ({ themeColors, setColor, color }) => {
-  const opacityStyle = {
-    opacity: 0.5
-  }
   
-  // const sizeStyle = {
-  //   size: 
-  // }
   return (
     <div className="container-dominant-color">
       <h2>Quelle est la couleur dominante de votre charte graphique ?</h2>
@@ -16,8 +10,7 @@ const ChooseDominantColor = ({ themeColors, setColor, color }) => {
         <input
           type="button"
           key={themeColor.value}
-          style={{ background: themeColor.hexadecimalColor }}
-          // value={themeColor.hexadecimalColor === color ? 'X': style=opacityStyle}
+          style={themeColor.hexadecimalColor === color ? {background: themeColor.hexadecimalColor, height: "5rem", width: "5rem"} : {background: themeColor.hexadecimalColor, opacity: 0.45}}
           onClick={(e) => setColor(themeColor.hexadecimalColor)}
         />
       ))}
