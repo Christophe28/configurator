2f3bd942f2ceedff038f7de9160838769bde54a7
christophe <christophebuffe@gmail.com>
2022-05-17 16:23:45 +0200

    vertical align for all pages ok

--- src/components/wizzardComponents/pages/choose-dominant-colors.jsx
+++ src/components/wizzardComponents/pages/choose-dominant-colors.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 const ChooseDominantColor = ({ themeColors, setColor, color }) => {
-  
   return (
     <div className="container-dominant-color">
       <h2>Quelle est la couleur dominante de votre charte graphique ?</h2>
@@ -10,7 +9,15 @@ const ChooseDominantColor = ({ themeColors, setColor, color }) => {
         <input
           type="button"
           key={themeColor.value}
-          style={themeColor.hexadecimalColor === color ? {background: themeColor.hexadecimalColor, height: "5rem", width: "5rem"} : {background: themeColor.hexadecimalColor, opacity: 0.45}}
+          style={
+            themeColor.hexadecimalColor === color
+              ? {
+                  background: themeColor.hexadecimalColor,
+                  height: "5rem",
+                  width: "5rem",
+                }
+              : { background: themeColor.hexadecimalColor, opacity: 0.45 }
+          }
           onClick={(e) => setColor(themeColor.hexadecimalColor)}
         />
       ))}
